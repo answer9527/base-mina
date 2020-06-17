ce670fac5f93f00204a0daede14de3f3a63dc8ef
answer9527 <516994147.com>
2020-06-17 08:51:16 +0800

    061701

---
+++ config/config.js
@@ -0,0 +1,7 @@
+const config = {
+    base_url:"http://127.0.0.1:8068/v1"
+   
+  }
+  export{
+    config
+  }
---
+++ models/classic.js
@@ -0,0 +1,23 @@
+import {Http} from "../utils/http"
+
+class ClassicModel{
+  // 获取上一篇或下一篇推荐
+  static async getClassic(index,nextOrPre){
+    let str = nextOrPre=="next"?"/next":"previous";
+    let res = await Http.request({
+      url:"/classic/"+index+str
+    })
+
+    return res;
+  }
+
+  // 获取最新的推荐
+  static async getLatest(){
+    return await Http.request({
+      url:"/classic/latest"
+    })
+  }
+}
+export{
+  ClassicModel
+}
