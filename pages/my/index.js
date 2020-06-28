dfdb72c05c4bc784f2af03742e92a6c1528642ee
answer9527 <516994147.com>
2020-06-28 08:48:13 +0800

    062801

--- pages/my/index.js
+++ pages/my/index.js
@@ -7,14 +7,68 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabList:[
+      {
+        "tabName": "我的喜欢",
+        "tabImage": "./images/like@tag.png",
+        "tagUrl": ""
+      },
+      {
+        "tabName": "我的消息",
+        "tabImage": "./images/msg@tag.png",
+        "tagUrl": ""
+      },
+      {
+        "tabName": "时光邮局",
+        "tabImage": "./images/mail@tag.png",
+        "tagUrl": ""
+      },
+      {
+        "tabName": "树洞",
+        "tabImage": "./images/talk@tag.png",
+        "tagUrl": ""
+      }
+    ],
+    colList:[
+      {
+        "colName":"申请加入",
+        "colImage":"./images/setting@col.png",
+        "colUrl":""
+      },
+      {
+        "colName": "意见反馈",
+        "colImage": "./images/idea@col.png",
+        "colUrl": ""
+      },
+      {
+        "colName": "关于我们",
+        "colImage": "./images/about@col.png",
+        "colUrl": ""
+      },
+      {
+        "colName": "版本说明",
+        "colImage": "./images/version@col.png",
+        "colUrl": ""
+      },
+      {
+        "colName": "联系我们",
+        "colImage": "./images/contact@col.png",
+        "colUrl": ""
+      }
+    ],
+    userInfo: {},
+    hasUserInfo: true,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(app.globalData)
+    this.setData({
+      userInfo:app.globalData.userInfo
+    })
+
   },
 
   /**
