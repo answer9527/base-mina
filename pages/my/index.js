2a8dd3a5ee06319f775ad3fea959a59ed3d1f7ea
answer9527 <516994147.com>
2020-06-29 08:58:04 +0800

    062901

--- pages/my/index.js
+++ pages/my/index.js
@@ -1,6 +1,7 @@
 // pages/my/index.js
 const app  =  getApp();
-
+import {UserModel} from "../../models/user"
+import {promisic2} from "../../utils/util"
 Page({
 
   /**
@@ -21,7 +22,7 @@ Page({
       {
         "tabName": "时光邮局",
         "tabImage": "./images/mail@tag.png",
-        "tagUrl": ""
+        "tagUrl": "/pages/beforeMessage/index"
       },
       {
         "tabName": "树洞",
@@ -57,17 +58,22 @@ Page({
       }
     ],
     userInfo: {},
-    hasUserInfo: true,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    default_avatar:"/images/default_avatar.png"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      userInfo:app.globalData.userInfo
-    })
+    if(app.globalData.userInfo){
+      this.setData({
+        userInfo:app.globalData.userInfo,
+        hasUserInfo:true
+      })
+    }
+
 
   },
 
@@ -118,5 +124,25 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  async getUserInfo(e){
+    let userInfo = e.detail.userInfo
+    let session = await promisic2(wx.login)();
+    let result = await UserModel.reigsterAndLogin({
+      code:session.code,
+      nickName:userInfo.nickName,
+      avatarUrl:userInfo.avatarUrl,
+      gender:userInfo.gender
+    })
+    wx.lin.showToast({
+      title:"登录成功",
+      icon:"success"
+    })
+    app.globalData.userInfo=e.detail.userInfo
+    app.globalData.token = result.data.token
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true  
+    })
   }
 })
