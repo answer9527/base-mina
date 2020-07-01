4c0534b92aaa12a9d063fcd695df7f759793b6ee
answer9527 <516994147.com>
2020-07-01 09:50:37 +0800

    070101

---
+++ pages/writeLetter/index.js
@@ -0,0 +1,94 @@
+// pages/writeLetter/index.js
+var dateTimePicker = require('../../utils/dateTimePicker.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    date: '2018-10-01',
+    time: '12:00',
+    dateTime: null,
+    dateTimeArray: null,
+    startYear: 2020,
+    endYear: 2050,
+    day: '',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var obj = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    var time = dateTimePicker.getHourMinu();
+    obj.dateTime[2] = parseInt((obj.defaultDay).substring(0, 2)) - 1; //day 字符串 'xx日' 转 'int'
+    this.setData({
+      dateTime: obj.dateTime,
+      dateTimeArray: obj.dateTimeArray,
+      day: obj.defaultDay,
+      time: time
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  changeDateTimeColumn(e) {
+
+    var arr = this.data.dateTime, dateArr = this.data.dateTimeArray;
+
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
+    //console.log(arr);
+    this.setData({
+      dateTimeArray: dateArr,
+      dateTime: arr,
+      day: dateArr[2][arr[2]].substring(0, 3),
+    });
+  }
+})
