3975683d2417b2f93dbf4ff670a6a122d24248a2
answer9527 <516994147.com>
2020-07-02 08:49:01 +0800

    070201

--- pages/writeLetter/index.js
+++ pages/writeLetter/index.js
@@ -9,11 +9,18 @@ Page({
   data: {
     date: '2018-10-01',
     time: '12:00',
+    // 年月日的序号
     dateTime: null,
     dateTimeArray: null,
     startYear: 2020,
     endYear: 2050,
     day: '',
+
+    // 表单动态绑定的内容
+    post_title:"",
+    post_content:"",
+    post_email:"",
+    plan_time:null
   },
 
   /**
@@ -27,7 +34,8 @@ Page({
       dateTime: obj.dateTime,
       dateTimeArray: obj.dateTimeArray,
       day: obj.defaultDay,
-      time: time
+      time: time,
+      plan_time:new Date()
     });
   },
 
@@ -81,22 +89,83 @@ Page({
   },
   // 设置时间的回调
   changeDateTimeColumn(e) {
-
+  
     var arr = this.data.dateTime, dateArr = this.data.dateTimeArray;
 
     arr[e.detail.column] = e.detail.value;
     dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
-    //console.log(arr);
+    let time_str= dateArr[0][arr[0]].substring(0,4)+"-"+dateArr[1][arr[1]].substring(0,2)+"-"+dateArr[2][arr[2]].substring(0, 2)+" 00:00:00"
+   
+    let plan_time = new Date(time_str)
+  
     this.setData({
       dateTimeArray: dateArr,
       dateTime: arr,
       day: dateArr[2][arr[2]].substring(0, 3),
+      plan_time:plan_time
     });
+  },
+  // 监听修改标题
+  changeTitle(e){
+    this.setData({
+      post_title:e.detail.value
+    })
+  },
+  // 监听修改书信内容
+  changeContent(e){
+    this.setData({
+      post_content:e.detail.value
+    })
+  },
+  // 监听修改邮箱
+  changeEmail(e){
+    this.setData({
+      post_email:e.detail.value
+    })
   },
   // 提交信件
   send_letter(){
-    LetterModel.insertLetter({}).then(res=>{
-      console.log(res)
+
+    let data = this.data
+    let param = {
+      title:data.post_title,
+      content:data.post_content,
+      email:data.post_email,
+      planTime:data.plan_time
+    }    
+    // 如果有空值
+    if(Object.values(param).indexOf("")>=0){
+      wx.showToast({
+        title: "请填写完整",
+        icon: 'none',
+        duration: 2000
+      })
+      return false
+    }
+  //  如果时间在今天之前
+    if(param.planTime<=new Date()){
+      wx.showToast({
+        title: "请填写正确时间",
+        icon: 'none',
+        duration: 2000
+      })
+      return false
+    }
+   
+
+    LetterModel.insertLetter(param).then(res=>{
+      wx.navigateTo({
+        url: '/pages/beforeLetter/index',
+        success: (result)=>{
+          wx.showToast({
+            title: "提交成功",
+            icon: 'none',
+            duration: 2000
+          })
+        },
+        fail: ()=>{},
+        complete: ()=>{}
+      });
     })
   }
 })
