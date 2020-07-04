984f7ad9fe17b2046d4bf09264be6d59392288d6
answer9527 <516994147.com>
2020-07-04 08:49:14 +0800

    070401

--- components/comment/index.js
+++ components/comment/index.js
@@ -1,5 +1,7 @@
 // components/comment/index.js
 import {CommentModel} from "../../models/comment"
+var appInst =  getApp();
+
 Component({
   /**
    * 组件的属性列表
@@ -20,7 +22,14 @@ Component({
    */
   methods: {
     // 点击
-    
+    showReplay(e){
+      let params={
+        "placeholder":"回复@"+e.currentTarget.dataset.usernamer+":",
+        "pid":e.currentTarget.dataset.pid,
+        "uid_r":e.currentTarget.dataset.uidr
+      }
+      this.triggerEvent("showReplay",params)
+    }
 
   },
   attached(){
--- pages/homeDetail/index.js
+++ pages/homeDetail/index.js
@@ -9,11 +9,21 @@ Page({
    * 页面的初始数据
    */
   data: {
+    
     classic:{},
     commentList:[],
     show_dialog:false,
+    // 评论框的内容
     comment_txt:"",
+    // 评论框placeholder的文字
+    placeholder:"文明发言,理性讨论！",
     ani:null,
+    // 回复评论的其他参数
+    comment_other:null,
+    cid:null,
+    page:1,
+    size:10
+
   },
 
   /**
@@ -24,12 +34,15 @@ Page({
     let id = options.id
     let classicDetail = await this.getClassicDetail(id)
     let paging = {
-      "key":id
+      "key":id,
+      "page":this.data.page,
+      "size":this.data.size
     }
     let commentList = await this.getClassicComment(paging)
     this.setData({
       classic:classicDetail,
-      commentList:commentList
+      commentList:commentList,
+      cid:id
     })
   },
 
@@ -57,8 +70,9 @@ Page({
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
+  onUnload:  function () {
+   
+   
   },
 
   /**
@@ -71,8 +85,21 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: async function () {
 
+    // 后期要修改分页的问题，减少请求次数
+    let page = this.data.page +1;
+    let paging = {
+      "key":this.data.cid,
+      "page":page,
+      "size":this.data.size
+    }
+    let commentList = await this.getClassicComment(paging)
+    commentList = this.data.commentList.concat(commentList)
+    this.setData({
+      commentList:commentList,
+      page:page
+    })
   },
 
   /**
@@ -86,8 +113,15 @@ Page({
       comment_txt:e.detail.value
     })
   },
-  // 弹出输入框
-  showDialog(){
+  // homeDetail页弹出输入框
+  showDialog(e){
+   
+    this.setData({
+      placeholder:"文明发言,理性讨论！",
+      comment_txt:"",
+      comment_other:{}
+    })
+    console.log(this.data.comment_other)
     var animation = wx.createAnimation({
       duration:800,
       timingFunction: 'ease',
@@ -98,8 +132,33 @@ Page({
       ani:  animation.export()
     })
   },
+  // 其他回复弹出的输入框
+  showReplay(e){
+    console.log(e)
+    this.setData({
+      placeholder:e.detail.placeholder,
+      comment_txt:"",
+      comment_other:{
+        pid:e.detail.pid,
+        uid_r:e.detail.uid_r
+      }
+    })
+    console.log(this.data.comment_other)
+    var animation = wx.createAnimation({
+      duration:800,
+      timingFunction: 'ease',
+    })
+    animation.height("360rpx").step()
+    this.setData({
+      show_dialog:true,
+      ani:  animation.export()
+    })
+  },
+  // 其他子评论点击后的弹出输入框
+
   // 隐藏输入框
   hideDialog(){
+    
     var animation = wx.createAnimation({
       duration:200,
       timingFunction: 'ease',
@@ -116,22 +175,41 @@ Page({
     return res.data
   },
   // 获取评论列表
-  async getClassicComment(cid){
-    let res = await CommentModel.getCommentByCid(cid)
+  async getClassicComment(paging){
+    let res = await CommentModel.getCommentByCid(paging)
     return res.data
   },
-  sendRootComment(){
-
-    CommentModel.insertComment({
+  // 提交评论
+  sendComment(){
+    let params={
       content:this.data.comment_txt,
       classicId:this.data.classic.id
-    }).then(res=>{
+     }
+     let temp =this.data.comment_other
+    // 判断comment_other是不是空对象
+    if(JSON.stringify(temp)!="{}"){
+      params = Object.assign(params,temp)
+    }
+
+    CommentModel.insertComment(params).then(res=>{
       wx.showToast({
         title: res.message,
         icon: 'none',
         duration: 2000
       })
+     
       this.hideDialog()
+      let paging ={
+        "key":this.data.cid,
+        "page":1,
+        "size":this.data.size
+      }
+      this.getClassicComment(paging).then(res=>{
+       this.setData({
+         page:1,
+         commentList:res
+       })
+      })
     })
   }
 
