94af913d3876148fd1281dfa1dc5b2c2248d99af
answer9527 <516994147.com>
2020-07-18 08:52:11 +0800

    “0718001

--- utils/http.js
+++ utils/http.js
@@ -4,11 +4,17 @@ import {promisic} from "./util"
 
 class Http{
   static async request({url,data,method="GET"}){
-    const token = wx.getStorageSync("token")||"";
+    let bearer_token;
+    const token = wx.getStorageSync("token");
+    if(token){
+      bearer_token="Bearer "+token
+    }else{
+      bearer_token=""
+    }
     let res = await promisic(wx.request)({
       header:{
         "context-type": "application/json",
-        "Authorization": "Bearer "+token
+        "Authorization": bearer_token
       },
       url:`${config.base_url}`+url,data,method
     })
