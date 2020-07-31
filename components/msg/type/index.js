9273d47233a02bf3f20a97e702749f62d07dd64f
answer9527 <516994147@qq.com>
2020-07-31 18:25:30 +0800

    0731002

--- components/msg/type/index.js
+++ components/msg/type/index.js
@@ -4,13 +4,24 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    type:{
+      type:Number
+    },
+    count:{
+      type:Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    classicIconSrc:"/images/classic_msg.png",
+    holeIconSrc:"/images/hole_msg.png",
+    sysIconSrc:"/images/sys_msg.png",
+    classicUrl:"/pages/msg/msgList/ClassicMsg/index",
+    holeUrl:"/pages/msg/msgList/HoleMsg/index",
+    sysUrl:"/pages/my/index"
 
   },
 
